3ffdfa5e49b5679894aaa469f9be2b1494d957f4
brakozyawr <brakozyawr@yandex.ru>
2022-07-14 23:59:03 +0300

    Исправление ошибок

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,13 +1,12 @@
-import MainScreen from '../../pages/main/main';
+import Main from 'src/pages/main/main';
 
 const title = 'The Grand Budapest Hotel';
 const genre = 'Drama';
 const year = '2014';
 
-function App(): JSX.Element {
-  return (
-    <MainScreen title={title} genre={genre} year={year}/>
-  );
-}
+const App = (): JSX.Element => (
+  <Main title={title} genre={genre} year={year}/>
+);
+
 
 export default App;
--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,4 +1,6 @@
-//это первоначальный вариант, чтобы все карточки фильмов были одинаковыми
+import {Film} from 'src/types/types';
+
+//это тупенький первоначальный вариант, чтобы все карточки фильмов были одинаковыми
 /*function FilmCard(): JSX.Element {
   return (
     <article className="small-film-card catalog__films-card">
@@ -12,54 +14,53 @@
   );
 }*/
 
-//это жалкая попытка передать в карточки оазные данные
-type Film = {
-  image: string,
-  alt: string,
-  title: string
-}
 
-type Props = {
-  filmEl:Film
-}
-
-//попытка итерироваться по массиву объектов с данными фильмов прямо в компоненте - не удалась, т.к. функция возврвщвлв
-//массив JSX.Element[], а так низзя, решение не дожала, интересно, как можно это сделать
-
+//вариант, когда итерация по элементам списка фильмов происходит извне, над самим компонентом карточки фильма, в main.tsx
 /*type Props = {
-  films:Film[]
-}*/
-
-/*function FilmCard({films}: Props): JSX.Element[] {
-  const filmsCard = films.map((film:Film) =>
-    (
-      <article key="" className="small-film-card catalog__films-card">
-        <div className="small-film-card__image">
-          <img src={film.image} alt={film.alt} width="280" height="175"/>
-        </div>
-        <h3 className="small-film-card__title">
-          <a className="small-film-card__link" href="film-page.html">{film.title}</a>
-        </h3>
-      </article>
-    )
-  );
-  return filmsCard;
-}*/
-
-function FilmCard({filmEl}: Props): JSX.Element {
+  film:Film
+}
+function FilmCard({film}: Props): JSX.Element {
   return (
     <article className="small-film-card catalog__films-card">
       <div className="small-film-card__image">
-        <img src={filmEl.image} alt={filmEl.alt} width="280" height="175"/>
+        <img src={film.image} alt={film.alt} width="280" height="175"/>
       </div>
       <h3 className="small-film-card__title">
-        <a className="small-film-card__link" href="film-page.html">{filmEl.title}</a>
+        <a className="small-film-card__link" href="film-page.html">{film.title}</a>
       </h3>
     </article>
   );
+}*/
+
+
+//вариант, когда итерация по элементам списка фильмов внутри компонента
+//ура, оно работает!!! )))
+
+type Props = {
+  films:Film[]
 }
 
+const FilmCards = ({films}: Props): JSX.Element => {
+  const filmCardList =
+    (
+      <>
+        {films.map((film:Film) =>
+          (
+            <article key="" className="small-film-card catalog__films-card">
+              <div className="small-film-card__image">
+                <img src={film.image} alt={film.alt} width="280" height="175"/>
+              </div>
+              <h3 className="small-film-card__title">
+                <a className="small-film-card__link" href="film-page.html">{film.title}</a>
+              </h3>
+            </article>
+          )
+        )};
+      </>
+    );
+  return filmCardList;
+};
 
-export default FilmCard;
+export default FilmCards;
 
 
